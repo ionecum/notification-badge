0eb94c9b5f27675c3f2fd9ae5450d30207b01454
Danil Izner <ionecum@gmail.com>
2023-12-23 01:44:12 -0500

    finished basic project

--- myproj/notifapi/views.py
+++ myproj/notifapi/views.py
@@ -1,7 +1,21 @@
+from django.conf import settings
+from django.http import HttpResponse
 from django.shortcuts import render
+import os, json
 
 def process_json(request):
-    pass
+    file_path = os.path.join(settings.BASE_DIR, "static/notifications_filtered.json")
+    with open(file_path, 'r', encoding='utf-8') as file:
+        #json.load can't be used, because there are multiple objects in the json
+        #json_data = json.load(file)
+        json_data = [json.loads(line) for line in file]
+    
+    # Convert the JSON data to JSON format (essentially making a copy here)
+    json_response = json.dumps(json_data)
+
+    return HttpResponse(json_response, content_type='application/json; charset=utf-8')
+
 
 def show_results(request):
-    pass
+    return render(request, "notifapi/demo.html")
+
