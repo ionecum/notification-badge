31188a52a9830f32b4261c5d6264176f309ae98a
Danil Izner <ionecum@gmail.com>
2024-02-26 02:19:13 -0500

    02-26-b

--- myproj/notifapi/signals.py
+++ myproj/notifapi/signals.py
@@ -2,23 +2,29 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-from channels.db import database_sync_to_async
 from .models import NotifyModel as Notification
-
+import asyncio
 
 @receiver(post_save, sender=Notification)
 def notification_created(sender, instance, created, **kwargs):
-    try:
-        if created:
-            print(f"A new notification was created {instance.message}")
-            channel_layer = get_channel_layer()
-            database_sync_to_async(channel_layer.group_send)(
-                "public_room",
-                {
+    
+    if created:
+        print(f"A new notification was created {instance.message}")
+        channel_layer = get_channel_layer()
+        print(channel_layer)
+        try:
+            async def send_notification():
+                await channel_layer.group_send("public_room", {
+                    # OJO, update_notification_count nunca es llamada
+                    # ese es el verdadero problema. Solo es llamada después
+                    # de refrescar con F5
                     "type": "update_notification_count",
                     "message": instance.message
-                }
-            )
-    except Exception as e:
-        print(f"Error in group_send: {e}")
+                })
+
+            # Call the async function to send the notification
+            async_to_sync(send_notification)()
+            
+        except Exception as e:
+            print(f"Error in group_send: {e}")
 
