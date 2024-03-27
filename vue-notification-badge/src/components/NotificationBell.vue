4240b3b3379abdeb51d6f1825f570a6c5ec3e7d9
ionecum <ionecum@gmail.com>
2024-03-27 12:01:57 -0400

    update read state missing

--- vue-notification-badge/src/components/NotificationBell.vue
+++ vue-notification-badge/src/components/NotificationBell.vue
@@ -1,17 +1,28 @@
 <template>
     <div class="fancy-container">
-        <a href="#" class="position-relative" @click="toggleNotificationMenu">
-            <i class="fa fa-bell _gray" style="font-size:24px"></i>
-            <span class="my-text btnLnk">Visits</span>
-            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger _reduced">
-                {{ notificationCount }}
-            </span>
-
-        </a>
+        <div class="notification-container" @click="toggleNotificationMenu">
+            <a href="#" class="position-relative" >
+                <i class="fa fa-bell _gray" style="font-size:24px"></i>
+                <span class="my-text btnLnk">Visits</span>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger _reduced">
+                    {{ notificationCount }}
+                </span>
+            </a>
+        
         <!-- Dynamic component to display notifications list -->
-        <component :is="showNotifications ? 'NotificationsList' : 'div'" :notifications="notifications" v-if="notifications.length > 0" @close="closeNotificationMenu" />
+        <component 
+            :is="showNotifications ? 'NotificationsList' : 'div'" 
+            :notifications="notifications" 
+            v-if="notifications.length > 0" 
+            @close="closeNotificationMenu" 
+        />
+        <!-- <a v-if="notifications.length > 20 && showNotifications" @click="showAllNotifications" style="cursor: pointer; color: blue; text-decoration: underline;">See More</a> -->
+    </div>
     </div>
 </template>
+<style>
+
+</style>
 <script>
 //import Axios from 'axios';
 import NotificationsList from './NotificationList.vue';
@@ -19,7 +30,6 @@ export default {
     data() {
         return {
             notifications: [],
-            
             webSocket: null,
             showNotifications: false, // Flag to toggle showing notifications list
         };
@@ -31,15 +41,33 @@ export default {
         }
     },
     mounted() {
+        document.addEventListener('click', this.handleClickOutside);
+        this.$el.querySelector('.notification-container').addEventListener('click', this.handleMenuClick);
         this.establishWebSocketConnection();
+    },
+    beforeUnmount() {
+        document.removeEventListener('click', this.handleClickOutside);
+        this.$el.querySelector('.notification-container').removeEventListener('click', this.handleMenuClick);
     },
     methods: {
         toggleNotificationMenu() {
-            console.log('Notifications data in parent component:', this.notifications);
             this.showNotifications = !this.showNotifications;
-            },
+        },
         closeNotificationMenu() {
             this.showNotifications = false; // Hide the notifications list
+        },
+        handleClickOutside(event) {
+            if (!this.$el.contains(event.target)) {
+                this.showNotifications = false;
+            }
+        },
+        handleMenuClick(event) {
+            event.stopPropagation(); // Prevent the click event from bubbling up
+        },
+        handleNotificationClick(notification) {
+            notification.is_read = true; // Mark the notification as read
+            // Emit an event to notify the parent component about the notification being read
+            this.$emit('notification-read', notification);
         },
 
         async establishWebSocketConnection() {
@@ -51,7 +79,7 @@ export default {
             };
             
             this.webSocket.onmessage = (event) => {
-                console.log("Message received:", event.data);
+                //console.log("Message received:", event.data);
                 
                 const message = JSON.parse(event.data);
                 //console.log("Event is:", event);  // Log the type field to identify the message type
@@ -74,7 +102,7 @@ export default {
             };
         },
         updateNotificationCount() {
-            console.log('updateNotificationCount called!');
+            //console.log('updateNotificationCount called!');
             // Send a message to the WebSocket server to request updated notification count
             this.webSocket.send(JSON.stringify({
                 "type": "update.notification.count"  // Define a custom type to trigger the count update
--- vue-notification-badge/src/components/NotificationList.vue
+++ vue-notification-badge/src/components/NotificationList.vue
@@ -1,39 +1,98 @@
 <template>
+  <div class="notifications-wrapper" @click="handleMenuClick">
     <div class="notifications-list">
+      <a href="#">Mark All as Read</a>
       <ul>
-        <li v-for="(notification, index) in notifications" :key="index">
-        {{ notification.message }}
-      </li>
+        <li v-for="(notification, index) in notifications.slice(0, 10)" :key="index">
+          <div :class="{ 'notification': true, 'bold': !notification.is_read }">
+            {{ notification.message }}
+          </div>
+        </li>
       </ul>
+      <div v-if="notifications.length > 10" class="see-more-link">
+            <a href="#" @click="showAllNotifications">See More</a>
+      </div>
+      
     </div>
+  </div>
   </template>
   
   <script>
   export default {
     props: ['notifications'],
+    data() {
+        return {
+            visibleNotifications: [],
+            showMoreLink: false,
+        };
+    },
     mounted() {
-      console.log('Notifications in child component:', this.notifications);
-    }
+      document.addEventListener('click', this.handleClickOutside);
+    },
+    beforeUnmount() {
+      document.removeEventListener('click', this.handleClickOutside);
+    },
+    methods: {
+        showAllNotifications() {
+            // Show all notifications when 'See More' is clicked
+            this.visibleNotifications = this.notifications;
+            this.showMoreLink = false; // Hide 'See More' link after displaying all notifications
+        },
+        handleMenuClick(event) {
+            event.stopPropagation(); // Prevent the click event from bubbling up
+        },
+        markAsRead(notification) {
+            notification.is_read = true;
+        },
+        handleClickOutside(event) {
+          if (!this.$el.contains(event.target)) {
+            this.visibleNotifications = [];
+            this.showMoreLink = false;
+          }
+        },
+    },
   };
   </script>
   
   <style>
-  .notifications-list {
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    padding: 10px;
-    background-color: #f9f9f9;
+    .notifications-wrapper {
+      position: relative;
+      display: inline-block;
+    }
+
+    .notifications-list {
+      position: absolute;
+      top: 100%;
+      left: -15;
+      width: 300px;   
+      max-height: 80vh;
+      overflow-y: auto;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      margin:0;padding:0;
+      background-color: #f9f9f9;
   }
+
+  .see-more-link {
+      margin: 10px 0;
+  }
+
   .notifications-list ul {
-    list-style-type: none;
-    padding: 0;
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
   }
+
   .notifications-list li {
-    margin-bottom: 5px;
-    padding: 5px;
-    background-color: #fff;
-    border: 1px solid #e1e1e1;
-    border-radius: 3px;
+      margin-bottom: 5px;
+      padding: 5px;
+      background-color: #fff;
+      border: 1px solid #e1e1e1;
+      border-radius: 3px;
   }
+  .bold {
+    font-weight: bold;
+  }
+
   </style>
   
