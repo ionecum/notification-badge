124284e2d3f664d9fd8807c83a8d9e14f7424dd9
ionecum <ionecum@gmail.com>
2024-04-17 00:33:54 -0400

    aftermatch

--- myproj/myproj/asgi.py
+++ myproj/myproj/asgi.py
@@ -2,10 +2,11 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from notifapi.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproj.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
+    "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
 })
